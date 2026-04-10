52d4816b82ed6844e5cc730417ebc2a34ff8cfc9
Rayan-1737 <apple@example.com>
2026-04-10 10:36:32 +0530

    Auto-sync: file changes

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+
+function App() {
+  return (
+    <div>
+      <img src={reactLogo} alt="React logo" width="120" />
+      <h1>Hello React</h1>
+    </div>
+  )
+}
+
+export default App
+
